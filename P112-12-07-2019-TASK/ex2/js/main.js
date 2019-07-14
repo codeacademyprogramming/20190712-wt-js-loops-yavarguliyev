f383d6e715f74727720952a066d2a50536dabe31
yavarguliyev <yavarrg@code.edu.az>
2019-07-14 21:44:35 +0400

    secondexerciseisdone

--- P112-12-07-2019-TASK/ex2/js/main.js
+++ P112-12-07-2019-TASK/ex2/js/main.js
@@ -8,7 +8,7 @@ function compare(num1, num2){
     if(num1 < 0 || num2 < 0){
         return (`${num1 + 0.5} && ${num2 + 0.5}`); 
     }
-    if(num1 != [0.5, 2.0] && num2 != [0.5, 2.0] ) {
+    if(!(num1 >= 0.5, 2.0 && num2 <= 2 )) {
         return num1 / 10 + " " + num2 / 10;
     }
 }
