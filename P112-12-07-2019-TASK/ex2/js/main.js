0ebecc7b89390c86cca411ff67acc70677b54c51
yavarguliyev <yavarrg@code.edu.az>
2019-07-14 16:44:29 +0400

    secondexerciseisdone

---
+++ P112-12-07-2019-TASK/ex2/js/main.js
@@ -0,0 +1,18 @@
+"use strict";
+
+function compare(num1, num2){
+
+    if (num1 < 0 && num2 < 0) {
+        return (num1 * (-1) + " " + num2 * (-1));
+    }
+    if(num1 < 0 || num2 < 0){
+        return (`${num1 + 0.5} && ${num2 + 0.5}`); 
+    }
+    if(num1 != [0.5, 2.0] && num2 != [0.5, 2.0] ) {
+        return num1 / 10 + " " + num2 / 10;
+    }
+}
+
+console.log( compare(-3, -6) );
+console.log( compare(-2, 4) );
+console.log( compare(5, 8) );
